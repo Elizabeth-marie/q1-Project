d53390969fada280cc4bad5904d0bccee08dbc11
Elizabeth-Marie <elizabeth.brown843@gmail.com>
2018-09-17 16:56:06 -0600

    added files

---
+++ Tests/tests.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+console.log("here!")
+
+
+//Enter Button event listener
+// let buttonC = document.getElementById("BButton")
+// buttonC.addEventListener("click", (event) => {
+// console.log('clicked!')
+//
+// })
+})
